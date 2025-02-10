ca3e79945813839925d37d01ffa597dad35a7bef
MannyR52 <119136589+MannyR52@users.noreply.github.com>
2025-02-10 20:04:36 +0000

    Added function to help run built in commands for requirement #3

--- ramirman_assignment4.c
+++ ramirman_assignment4.c
@@ -2,10 +2,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define INPUT_LENGTH 2048
 #define MAX_ARGS	 512
 
+int last_status = 0;                // Variable to track last fg process status
 
 // Structure holds parsed command info (from provided sample_parser code)
 struct command_line
@@ -57,6 +61,47 @@ struct command_line *parse_input()
 	return curr_command;
 }
 
+
+// Function to help run built-in commands
+bool run_builin(struct command_line *cmd)
+{
+    if (cmd->argc == 0)     // Returns false if no command is input
+    {
+        return false;
+    }
+
+    if (strcmp(cmd->argv[0], "exit") == 0)      // Handles exit command
+    {
+        exit(0);
+    }
+
+    if (strcmp(cmd->argv[0], "cd") == 0)
+    {
+        char *path = (cmd->argc > 1) ? cmd->argv[1] : getenv("HOME");
+        
+        if (chdir(path) != 0)
+        {
+            perror("cd");
+        }
+        return true;
+    }
+
+    if (strcmp(cmd->argv[0], "status") == 0)
+    {
+        if (WIFEXITED(last_status))
+        {
+            printf("exit value %d\n", WEXITSTATUS(last_status));
+        }
+        else
+        {
+            printf("terminated by signal %d\n", WTERMSIG(last_status));
+        }
+        fflush(stdout);
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
 	struct command_line *curr_command;
@@ -69,6 +114,12 @@ int main()
         if (!curr_command)              
         {
             continue;
+        }
+
+        // Runs built in commands
+        if (!run_builin(curr_command))
+        {
+            printf("Executing external command: %s\n", curr_command->argv[0]);
         }
 
         // For loop frees memory for parsed command
